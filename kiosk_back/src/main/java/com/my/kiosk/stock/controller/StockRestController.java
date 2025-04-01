d20a157db45fc13a765f4f79426e83535af92016
전진훈 <jin941111@naver.com>
2025-04-01 10:43:42 +0900

    test

--- kiosk_back/src/main/java/com/my/kiosk/stock/controller/StockRestController.java
+++ kiosk_back/src/main/java/com/my/kiosk/stock/controller/StockRestController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.my.kiosk.stock.classes.Stock;
@@ -85,7 +86,12 @@ public ResponseEntity<?> stockInOutList() {
     } catch (Exception e) {
         e.printStackTrace();
         return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
-    }
+    	}
+	}
 }
+/*
+public List<Stcok> getBydate (@RequestParam String startdate)
+
 	
 }
+ */
